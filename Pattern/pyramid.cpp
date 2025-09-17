86e02246cfa9bacdd0cba62a6630740988979df3
Tejas Padhiyar <wableanushka@gmail.com>
2025-09-17 23:43:19 +0530

    pyramid pattern

---
+++ Pattern/pyramid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        // print spaces before stars
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+
+        // print stars
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+*/
