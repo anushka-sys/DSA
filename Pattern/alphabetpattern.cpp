bb7afae98bd56bb4dbeac98885cb413fc369023e
Tejas Padhiyar <wableanushka@gmail.com>
2025-09-20 23:36:07 +0530

    commit

---
+++ Pattern/alphabetpattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        cout << "A" << endl;
+    }
+    return 0;
+}
+/*
+A
+A
+A
+A
+A
+*/
